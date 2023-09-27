59b7ed9a6568adc0f70f8c1bf255efb0dea04bff
vignesh <vigneshwar.sigamani@gmail.com>
2023-09-27 06:38:09 +0530

    completed dom

--- javascript_practise/DOM_understanding/index.js
+++ javascript_practise/DOM_understanding/index.js
@@ -12,7 +12,7 @@ console.log(firstElementH1Element.innerText);
 
 // ======== CREATING ELEMENTS ====================
 /**
- * ------- this used here
+ * ------- this used here   
  * createElement()
  * getElementById()
  * appendChild()
@@ -30,8 +30,75 @@ function addNewFact(factText){
 addNewFact("innerText property refers to the text inside an element");
 addNewFact("append() or appendChild() methods can add a new child element");
 
-// =========== OVERWRITING ELEMENTS ( REPLACING THE EXISTING ELEMENT )===========
+// =========== OVERWRITING ELEMENTS ( REPLACING THE EXISTING ELEMENT ) ===========
+/**
+ * innerHTML
+ */
+
+const factsList = document.getElementsByTagName("facts-list");
+factsList.innerText = "<p>we are adding something </p><p>this is the second item in the list</p>"
+// const firstElementH1Element = h1Element[0];
+// console.log(firstElementH1Element);
+// console.log(firstElementH1Element.innerText);
+
+// ============== CREATING MULTIPLE DOM ELEMENTS ============
 /**
  * 
  */
 
+const tableElementDiv = document.getElementById("table-div")
+console.log(tableElementDiv);
+tableElementDiv.innerHTML = `
+<table border="1" width="400px"> 
+<thead> 
+    <tr>
+        <td>Id</td>
+        <td>Name</td>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td>1</td>
+    <td>Ramesh</td>
+    </tr>
+</tbody>
+</table>
+
+`
+
+// creating table with a function
+
+
+function loadTable() {
+    // Create <table> element with styles
+    let table = document.createElement("table");
+    table.setAttribute("border", "solid");
+    table.setAttribute("width", "400px");
+    // Create header row and append it to the <table> element
+    let thead = document.createElement("thead");
+    thead.innerHTML = `
+  
+      <tr>
+          <th>Id</th>
+          <th>Name</th>
+      </tr>
+  
+    `;
+    table.append(thead);
+    // Create body element
+    let tbody = document.createElement("tbody");
+    // Create a table row and append it to the body
+    let tRow = document.createElement("tr");
+    tRow.innerHTML = `
+      <td>1</td>
+      <td>Rajesh</td>
+    `;
+    // Append <tr> element to <tbody>
+    tbody.appendChild(tRow);
+    // Append <tbody> element to <table>
+    table.appendChild(tbody);
+    // Set newly created <table> element inside the div container
+    document.getElementById("table-div").append(table);
+  }
+  loadTable()
+  
